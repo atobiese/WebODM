425dafcb1ad5a4f412a084ca12ce54bdebb03168
atobiese <atobiese@yahoo.no>
2023-03-18 13:34:14 +0100

    copy xmp data to the resized destination image

--- app/models/task.py
+++ app/models/task.py
@@ -82,6 +82,41 @@ def validate_task_options(value):
         raise ValidationError("Invalid options")
 
 
+def add_xmp(source: str, dest: str):
+    """
+
+    :param source: absolute name of source image file (string)
+    :param dest: absolute name of destination image file (string)
+    :return: error message
+    """
+    with open(source, 'r+b') as file_1:
+        o_img = file_1.read()
+
+        xmp_start = o_img.find(b'http://ns.adobe.com/xap/1.0/\0')
+        xmp_end = o_img.find(b'\xff\xdb', xmp_start)
+
+        if xmp_start == -1:
+            print(f"Warning: There are no XMP data available in the current image: {source}.")
+            return False
+
+        xmp_str = o_img[xmp_start - 4: xmp_end]
+
+    with open(dest, 'r+b') as file_2:
+        d_img = file_2.read()
+
+        xmp_end = d_img.find(b'\xff\xdb')
+
+        first_part = d_img[:xmp_end]
+        second_part = d_img[xmp_end:]
+
+        new_str = first_part + xmp_str + second_part
+
+        file_2.seek(0)
+        file_2.truncate()
+        file_2.write(new_str)
+        file_2.close()
+
+    return True
 
 def resize_image(image_path, resize_to, done=None):
     """
@@ -149,6 +184,11 @@ def resize_image(image_path, resize_to, done=None):
             im.save(resized_image_path, **params)
 
         im.close()
+
+        # copy xmp data if it is available
+        source = image_path
+        dest = resized_image_path
+        add_xmp(source, dest)
 
         # Delete original image, rename resized image to original
         os.remove(image_path)
